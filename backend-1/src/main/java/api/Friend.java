e6cec89a2787d259b2714d052e18750ee9f6924c
rsk5492 <ssk5492@gmail.com>
2017-04-29 01:11:59 -0700

    created basic backend

---
+++ backend-1/src/main/java/api/Friend.java
@@ -0,0 +1,12 @@
+package api;
+
+/**
+ * Created by Srinivas on 4/25/2017.
+ */
+
+    public class Friend {
+        // manual
+        private long _id;
+
+    }
+
---
+++ backend-1/src/main/java/api/index.java
@@ -0,0 +1,9 @@
+package api;
+
+import org.restlet.resource.ServerResource;
+
+/**
+ * Created by Srinivas on 4/25/2017.
+ */
+public class index extends ServerResource{
+}
